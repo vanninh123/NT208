0a3ceaa75ccb0b9a6abe3df669aab339657c8db1
Huy <19521637@gm.uit.edu.vn>
2024-05-21 15:23:32 +0700

    them file bookmark

--- backend/main-api/app.py
+++ backend/main-api/app.py
@@ -1,17 +1,15 @@
-#!venv/bin/python
 from flask import Flask
-
 from blueprints.archive import archive
+from blueprints.bookmark import bookmark_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(archive, url_prefix="/archive")
-
+app.register_blueprint(bookmark_bp, url_prefix="/bookmark")
 
 @app.route('/')
 def index():
     return "You really shouldn't be here..."
 
 if __name__ == '__main__':
-    #app.run(debug=True)
     app.run(host='0.0.0.0', port='5000')
